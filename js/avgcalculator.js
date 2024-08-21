d790ad42024c29aa20a3d323da6bd04ffff6137c
Aleks <boberaleksander08@gmail.com>
2024-08-21 09:17:42 +0000

    Add files via upload

--- js/avgcalculator.js
+++ js/avgcalculator.js
@@ -26,7 +26,8 @@ const formatop2 = document.getElementById('formatop2');
 
 let time = [];
 let timebig = [];
-let timepenalty = [1, 1, 1, 1, 1]; // 0 = ok, 1 = +2, 2 = dnf
+let timepenalty = [1, 1, 1, 1, 1]; // 1 = ok, 2 = +2, 3 = dnf
+let timednf = [0, 0, 0, 0, 0];
 let timebigtemp = 0;
 let bpax = 0;
 let wpax = 0;
@@ -52,11 +53,17 @@ let targetavgstring = '';
 let targetchuj = 0;
 let targetxtime = 0;
 let avgxbig = 0;
+let bestplace = 0;
+let worstplace = 0;
 let bpaxtime = 0;
 let wpaxtime = 0;
+let isnewbest = 0;
+let isnewworst = 0;
+let newbest = 0;
+let newworst = 0; // for +2 and dnf
 let format = 1; // 1 = ao5, 2 = mo3
 let bestex = 0;
-let worstex = 0; // A monument to the total failure when I for some reason thought that after there's a new best time, that you have to check wether or not it's the new worst time and vice versa
+let worstex = 0;
 
 // Keybindings
 function handleShortcut(event) {
@@ -180,23 +187,28 @@ function processBestWorstTwo() {
 	if (time[1] < bestx && timebig[1] == 0 && timebig[0] == 0) {
 		worstx = bestx;
 		bestx = time[1];
+		bestplace = 1;
 		best.innerText = 'Best:  ' + time[1].toFixed(2);
 		worst.innerText = 'Worst:  ' + worstx.toFixed(2);
 	} else if (time[1] < bestx && timebig[1] == 0 && timebig[0] > 0) {
 		worstx = bestx;
 		bestx = time[1];
+		bestplace = 1;
 		best.innerText = 'Best:  ' + time[1].toFixed(2);
 		worst.innerText = 'Worst:  ' + timebig1;
 	} else if (time[1] < bestx && timebig[1] > 0 && timebig[0] > 0) {
 		worstx = bestx;
 		bestx = time[1];
+		bestplace = 1;
 		best.innerText = 'Best:  ' + timebig2;
 		worst.innerText = 'Worst:  ' + timebig1;
 	} else if (time[1] > worstx && timebig[1] == 0) {
 		worstx = time[1];
+		worstplace = 1;
 		worst.innerText = 'Worst:  ' + time[1].toFixed(2);
 	} else if (time[1] > worstx && timebig[1] > 0) {
 		worstx = time[1];
+		worstplace = 1;
 		worst.innerText = 'Worst:  ' + timebig2;
 	}
 };
@@ -204,10 +216,12 @@ function processBestWorstTwo() {
 function processBestWorst() {
 	if (time[timecount] < bestx && timebig[timecount] == 0) {
 		bestx = time[timecount];
+		bestplace = timecount;
 		best.innerText = 'Best:  ' + time[timecount].toFixed(2);
 		processBPA();
 	} else if (time[timecount] > worstx && timebig[timecount] == 0) {
 		worstx = time[timecount];
+		worstplace = timecount;
 		worst.innerText = 'Worst:  ' + time[timecount].toFixed(2);
 		processBPA();
 	} else if (time[timecount] < bestx && timebig[timecount] > 0) {
@@ -223,26 +237,32 @@ function processBestWorst() {
 function processBestWorstBig() {
 	if (timecount == 2 && time[2] < bestx) {
 		bestx = time[2];
+		bestplace = 2;
 		best.innerText = 'Best:  ' + timebig3;
 		processBPA();
 	} else if (timecount == 2 && time[2] > worstx) {
 		worstx = time[2];
+		worstplace = 2;
 		worst.innerText = 'Worst:  ' + timebig3;
 		processBPA();
 	} else if (timecount == 3 && time[3] < bestx) {
 		bestx = time[3];
+		bestplace = 3;
 		best.innerText = 'Best:  ' + timebig4;
 		processBPA();
 	} else if (timecount == 3 && time[3] > worstx) {
 		worstx = time[3];
+		worstplace = 3;
 		worst.innerText = 'Worst:  ' + timebig4;
 		processBPA();
 	} else if (timecount == 4 && time[4] < bestx) {
 		bestx = time[4];
+		bestplace = 4;
 		best.innerText = 'Best:  ' + timebig5;
 		processBPA();
 	} else if (timecount == 4 && time[4] > worstx) {
 		worstx = time[4];
+		worstplace = 4;
 		worst.innerText = 'Worst:  ' + timebig5;
 		processBPA();
 	} else {
@@ -557,38 +577,124 @@ formatop2.onclick = function() {
 // Chooses the correct format
 function pickFormat1() {
 	if (format === 1) {
-		timeOK();
+		checkOK();
 	} else if (format === 2) {
-		timeOKM();
+		checkOKM();
 	}
 };
 
 function pickFormat2() {
 	if (format === 1) {
-		timePlus2();
+		checlPlus2();
 	} else if (format === 2) {
-		timePlus2M();
+		checkPlus2M();
 	}
 };
 
 function pickFormat3() {
 	if (format === 1) {
-		timeDNF();
+		checkDNF();
 	} else if (format === 2) {
-		timeDNFM();
+		checkDNFM();
+	}
+};
+
+// Checks which penalty the previous time had
+function checkOK() {
+	if (timepenalty[timecount] == 2) {
+		timeOKPlus2();
+	} else if (timepenalty[timecount] == 3) {
+		timeOKDNF();
+	}
+};
+
+function checkPlus2() {
+	if (timepenalty[timecount] == 1) {
+		timePlus2OK();
+	} else if (timepenalty[timecount] == 3) {
+		timePlus2DNF();
+	}
+};
+
+function checkDNF() {
+	if (timepenalty[timecount] == 1) {
+		timeDNFOK();
+	} else if (timepenalty[timecount] == 2) {
+		timeDNFPlus2();
 	}
 };
 
 // Applies +2 or dnf to solve
-function timeOK() {
-	alert("Ok");
+function timeOKPlus2() {
+
 };
 
-function timePlus2() {
-	alert("+2");
+function timeOKDNF() {
+
 };
 
-function timeDNF() {
-	alert("DNF");
+function timePlus2OK() {
+	if (timecount == 0)
+};
+
+function timePlus2DNF() {
+
+};
+
+function timeDNFOK() {
+
+};
+
+function timeDNFPlus2() {
+
+};
+
+// All the same as above but for mo3
+function checkOKM() {
+	if (timepenalty[timecount] == 2) {
+		timeOKPlus2M();
+	} else if (timepenalty[timecount] == 3) {
+		timeOKDNFM();
+	}
+};
+
+function checkPlus2M() {
+	if (timepenalty[timecount] == 1) {
+		timePlus2OKM();
+	} else if (timepenalty[timecount] == 3) {
+		timePlus2DNFM();
+	{
+};
+
+function checkDNFM() {
+	if (timepenalty[timecount] == 1) {
+		timeDNFOKM();
+	} else if (timepenalty[timecount] == 2) {
+		timeDNFPlus2M();
+	}
+};
+
+function timeOKPlus2M() {
+
+};
+
+function timeOKDNFM() {
+
+};
+
+function timePlus2OKM() {
+
+};
+
+function timePlus2DNFM() {
+
+};
+
+function timeDNFOKM() {
+
+};
+
+function timeDNFPlus2M() {
+
 };
 });
