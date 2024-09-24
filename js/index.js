0905a5eea69743fb85d38b3a7b33f719d765f3ca
Aleks <boberaleksander08@gmail.com>
2024-09-24 09:26:37 +0000

    Write scramble

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const menu = document.getElementById('menu');
 const calc = document.getElementById('calc');
 const calcdiv = document.getElementById('calcdiv');
 const scramble = document.getElementById('scramble');
-const scramblediv = document.getElementById('scramlediv');
+const scramblediv = document.getElementById('scramblediv');
 const algs = document.getElementById('algs');
 const algsdiv = document.getElementById('algsdiv');
 const helpers = document.getElementById('helpers');
@@ -18,7 +18,7 @@ const calcmenubutton = document.getElementById('calcmenubutton');
 const scramblemenubutton = document.getElementById('scramblemenubutton');
 const algsmenubutton = document.getElementById('algsmenubutton');
 const helpersmenubutton = document.getElementById('helpersmenubutton');
-const menubuttonsettings = document.getElementById('menubuttonsettings');
+const settingsmenubutton = document.getElementById('settingsmenubutton');
 const css = document.getElementsByTagName('link');
 const lxa = document.getElementById('lxa');
 const hbody = document.getElementById('hbody');
@@ -51,32 +51,32 @@ calc.onclick = function() {
 	menu.style.display = 'none';
 	topmenu.style.display = 'none';
 	calcdiv.style.display = 'flex';
-	calcmenubutton.style.visibility = 'visible';
 	settings.style.visibility = 'collapse';
+	lxa.style.visibility = 'collapse';
 };
 
 scramble.onclick = function() {
 	menu.style.display = 'none';
 	topmenu.style.display = 'none';
 	scramblediv.style.display = 'flex';
-	scramblemenubutton.style.visibility = 'visible';
 	settings.style.visibility = 'collapse';
+	lxa.style.visibility = 'collapse';
 };
 
 algs.onclick = function() {
 	menu.style.display = 'none';
 	topmenu.style.display = 'none';
 	algsdiv.style.display = 'flex';
-	algsmenubutton.style.visibility = 'visible';
 	settings.style.visibility = 'collapse';
+	lxa.style.visibility = 'collapse';
 };
 
 helpers.onclick = function() {
 	menu.style.display = 'none';
 	topmenu.style.display = 'none';
 	helpersdiv.style.display = 'inline'; // will def change this when I'll actually build helpersdiv
-	helpersmenubutton.style.visibility = 'visible';
 	settings.style.visibility = 'collapse';
+	lxa.style.visibility = 'collapse';
 };
 
 competitiongroups.onclick = function() {
@@ -91,48 +91,49 @@ settings.onclick = function() {
 	menu.style.display = 'none';
 	topmenu.style.display = 'none';
 	settingsmenu.style.display = 'inline'; // will also change this when i build the settings
-	menubuttonsettings.style.visibility = 'visible';
+	settingsmenubutton.style.visibility = 'visible';
 	settings.style.visibility = 'collapse';
+	lxa.style.visibility = 'collapse';
 };
 
 calcmenubutton.onclick = function() {
 	menu.style.display = 'flex';
 	topmenu.style.display = 'flex';
 	calcdiv.style.display = 'none';
-	calcmenubutton.style.visibility = 'collapse';
 	settings.style.visibility = 'visible';
-	reset.style.visibility - 'collapse';
+	lxa.style.visibility = 'visible';
 };
-`
+
 scramblemenubutton.onclick = function() {
 	menu.style.display = 'flex';
 	topmenu.style.display = 'flex';
 	scramblediv.style.display = 'none';
-	scramblemenubutton.style.visibility = 'collapse';
 	settings.style.visibility = 'visible';
+	lxa.style.visibility = 'visible';
 };
-
+`
 algsmenubutton.onclick = function() {
 	menu.style.display = 'flex';
 	topmenu.style.display = 'flex';
 	algsdiv.style.display = 'none';
-	algsmenubutton.style.visibility = 'collapse';
 	settings.style.visibility = 'visible';
+	lxa.style.visibility = 'visible';
 };
 
 helpersmenubutton.onclick = function() {
 	menu.style.display = 'flex';
 	topmenu.style.display = 'flex';
 	helpersdiv.style.display = 'none';
-	helpersmenubutton.style.visibility = 'collapse';
 	settingss.style.visibility = 'visible';
+	lxa.style.visibility = 'visible';
 };`
 
-menubuttonsettings.onclick = function() {
+settingsmenubutton.onclick = function() {
 	menu.style.display = 'flex';
 	topmenu.style.display = 'flex';
 	settingsmenu.style.display = 'none';
-	menubuttonsettings.style.visibility = 'collapse';
+	settingsmenubutton.style.visibility = 'collapse';
 	settings.style.visibility = 'visible';
+	lxa.style.visibility = 'visible';
 };
 });
