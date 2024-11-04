71677597df1fce758ac51bba7bf160f9e7883616
Aleks <boberaleksander08@gmail.com>
2024-11-04 10:41:54 +0100

    update

---
+++ js/scramble.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("keydown", handleShortcut);
+
+
+
+function handleShortcut(event) {
+	if ((event.key === "" || event.key === "Enter")) {
+	
+	}
+}
+
+});
