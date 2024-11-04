bf8134a7f777e25ab6ff8f259e0102d1b91c4fd6
Aleks <boberaleksander08@gmail.com>
2024-11-04 13:41:25 +0100

    update

--- js/scramble.js
+++ js/scramble.js
@@ -1,12 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("keydown", handleShortcut);
 
-
+int currentEvent = 3; // NxN = N | 3OH, 3BLD, 3FM, 3MBLD = 33, 333, 3333 | Pyra, Skewb, Sq1, Mega, Clock = 10, 11, 12, 13, 14 | 4BLD, 5BLD = 444, 555
 
 function handleShortcut(event) {
 	if ((event.key === "" || event.key === "Enter")) {
 	
 	}
-}
+};
+
+function scramble() {
+	if (currentEvent == 2) {
+
+	} else if (currentEvent == 3) {
+
+	} else if (currentEvent == 4) {
+
+	} else if (currentEvent == 5) {
+
+	} else if (currentEvent == 6) {
+
+	} else if (currentEvent == 7) {
+
+	} else if (currentEvent == 10) {
+
+	} else if (currentEvent == 11) {
+
+	} else if (currentEvent == 12) {
+		
+	} else if (currentEvent == 13) {
+
+	} else if (currentEvent == 14) {
+
+	} else if (currentEvent == 33) {
+
+	} else if (currentEvent == 333) {
+
+	} else if (currentEvent == 3333) {
+
+	} else if (currentEvent == 444) {
+
+	} else if (currentEvent == 555) {
+
+	} else if (currentEvent == 33333) {
+
+	}
+};
 
 });
